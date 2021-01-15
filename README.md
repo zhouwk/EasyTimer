5667f2964f8c59977647d0f75a071c9f76b4e922
zhouwk <2963329733@qq.com>
2021-01-15 20:41:42 +0800

    0.0.1

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # EasyTimer
 
-A description of this package.
+不影响Target的释放，在Target的init中主动释放timer
--- Sources/EasyTimer/EasyTimer.swift
+++ Sources/EasyTimer/EasyTimer.swift
@@ -1,3 +1,59 @@
-struct EasyTimer {
-    var text = "Hello, World!"
+//
+//  EasyTimer.swift
+//  EasyTimer
+//
+//  Created by 周伟克 on 2020/10/5.
+//
+
+import Foundation
+
+public typealias EasyTimerTask = () -> ()
+
+public class EasyTimer {
+    
+    private let timer: Timer
+    private let target: EasyTimerTarget
+    
+    private init(timer: Timer, target: EasyTimerTarget) {
+        self.timer = timer
+        self.target = target
+    }
+
+    public static func set(fireDate date: Date = Date(),
+                           interval ti: TimeInterval,
+                           repeats rep: Bool = true,
+                           task t: @escaping EasyTimerTask) -> EasyTimer {
+        let target =  EasyTimerTarget(t)
+        let timer = Timer(fireAt: date,
+                          interval: ti,
+                          target: target,
+                          selector: #selector(target.execute),
+                          userInfo: nil,
+                          repeats: rep)
+        RunLoop.current.add(timer, forMode: .common)
+        return EasyTimer(timer: timer, target: target)
+    }
+    
+    deinit {
+        timer.invalidate()
+    }
+    
+    public func pause() {
+        timer.fireDate = .distantFuture
+    }
+    
+    public func resume() {
+        timer.fireDate = .init()
+    }
+}
+
+public class EasyTimerTarget {
+    var task: EasyTimerTask
+    public init(_ task: @escaping EasyTimerTask) {
+        self.task = task
+    }
+    
+    @objc func execute() {
+        task()
+    }
 }
